c831c7d96500b269df6e0cbb4caa4c190ce7e864
josexx78 <jose.oyarzun.v@usach.cl>
2019-07-04 21:55:04 -0400

    Se arreglo un error en programa_estudio

--- lccweb/apps/programa_estudio/models.py
+++ lccweb/apps/programa_estudio/models.py
@@ -4,6 +4,6 @@ from apps.asignatura.models import asignatura
 # Create your models here.
 class programa_estudio(models.Model):
 	codigo = models.CharField(primary_key=True,max_length=5)
-	año = models.IntegerField(4)
-	semestre = models.IntegerField(1)
+	anno = models.IntegerField()
+	semestre = models.IntegerField()
 	asig = models.OneToOneField(asignatura,on_delete=models.CASCADE)
