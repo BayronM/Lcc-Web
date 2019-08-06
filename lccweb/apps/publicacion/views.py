cfb44d5b408daa513a6b20a1db18c15205bdd861
Bayron Monsálvez <bayronmonsalvez@gmail.com>
2019-08-06 11:38:08 -0400

    paginacion noticias

--- lccweb/apps/publicacion/views.py
+++ lccweb/apps/publicacion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator
 
 from .models import publicacion,noticia, prueba
 from apps.estatico.models import Contenido_Estatico
@@ -14,23 +15,31 @@ def home_view(request, *args,**kwargs):
     return render(request,template_name , context)
 
 
-def news_view(request,*args,**kwargs):
-    obj=noticia.objects.order_by('-fecha').filter(estado=1)[:9]
-    content=Contenido_Estatico.objects.all()
+def news_view(request):
+    obj = publicacion.objects.order_by('-fecha').filter(estado=1)
+    paginator = Paginator(obj,9)
+    content = Contenido_Estatico.objects.all()
+    page = request.GET.get('page')
+    news = paginator.get_page(page)
+    context={
+    'noticias' : news,
+    'list_of_content' : content,
+    'pages' : range(news.paginator.num_pages)}
+
     template_name = "html/news.html"
-    context={'list_of_news' : obj,
-    'list_of_content' : content }
+
     return render(request,template_name,context)
 
 
 
 def news_view_detail(request , pub_id):
-    obj = get_object_or_404(publicacion, id=pub_id )
+    obj = get_object_or_404(noticia, id=pub_id,estado=1)
     objprueba=prueba.objects.filter(semestre=obj.calsemestre
                                     ,fecha__year=obj.calagno)
+    content=Contenido_Estatico.objects.all()
     template_name = "html/news_detail.html"
-    context = { "object": obj,
-               "Pruebas": objprueba,
+    context = { 'object': obj,
+               'Pruebas': objprueba,
                'list_of_content' : content }
     return render(request,template_name,context)
 
